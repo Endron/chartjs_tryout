53b3259f10a0526f547249345dcc1f65aec68eb7
Tobias Kremer <tobias.kremer@comsysto.com>
2015-03-10 15:57:53 +0100

    improve build by compressing html

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,18 @@
 var gulp = require('gulp')
 var jshint = require('gulp-jshint')
+var minifyHTML = require('gulp-minify-html');
 
 gulp.task('jshint', function() {
   gulp.src('./src/**/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
+});
+
+gulp.task('htmlpage', function() {
+  var htmlSrc = './src/**/*.html';
+  var htmlDst = './build';
+
+  gulp.src(htmlSrc)
+    .pipe(minifyHTML())
+    .pipe(gulp.dest(htmlDst));
 });
